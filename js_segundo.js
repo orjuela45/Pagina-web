4c4aa2cc7f5699761a80d6cf86fbcd9272f12584
orjuela45 <39718400+orjuela45@users.noreply.github.com>
2018-10-26 09:15:33 -0500

    implementacion js

---
+++ js_segundo.js
@@ -0,0 +1,10 @@
+$(".procesador").click(function() {
+    $(".informacion1 ").css({
+        background: "red",
+        display: "block",
+        width: "90%",
+        marginLeft: "4em",
+        marginTop:"1em"
+    }
+    )
+})
