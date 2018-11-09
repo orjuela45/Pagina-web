aa89d7eef7bb0ee367197275dc37cd1368389aea
orjuela45 <miguel.orjuela45@gmail.com>
2018-11-09 15:41:46 -0500

    fnalizacion de infomracion pagina 4 falta animaciones

--- js/js_cuarto.js
+++ js/js_cuarto.js
@@ -2,12 +2,22 @@ var numero=0;
 $(document).ready(function(){
     $(".mantenimiento").css("display", "none")
     $(".perifericos").css("display", "none")
+    $(".info_mantenimiento").css("display", "none")
+    $(".info_perifericos").css("display", "none")
+    $(".mouse").css("display", "none")
+    $(".teclado").css("display", "none")
+    $(".mouse-pad").css("display", "none")
+    $(".auriculares").css("display", "none")
+    $(".monitor").css("display", "none")
     $(".devolver").click(function(){
         if(numero==2)
         {
             $(".mantenimiento").css("display", "flex")
             $(".perifericos").css("display", "none")
             $(".montar").css("display", "none")
+            $(".info_montar").css("display", "none")
+            $(".info_mantenimiento").css("display", "flex")
+            $(".info_perifericos").css("display", "none")
             numero=1
         }
         else{
@@ -16,6 +26,9 @@ $(document).ready(function(){
                 $(".perifericos").css("display", "none")
                 $(".mantenimiento").css("display", "none")
                 $(".montar").css("display", "flex")
+                $(".info_montar").css("display", "flex")
+                $(".info_mantenimiento").css("display", "none")
+                $(".info_perifericos").css("display", "none")
                 numero=0
             }
             else{
@@ -24,6 +37,9 @@ $(document).ready(function(){
                     $(".mantenimiento").css("display", "none")
                     $(".perifericos").css("display", "flex")
                     $(".montar").css("display", "none")
+                    $(".info_montar").css("display", "none")
+                    $(".info_mantenimiento").css("display", "none")
+                    $(".info_perifericos").css("display", "flex")
                     numero=2
                 }
             }
@@ -35,6 +51,9 @@ $(document).ready(function(){
             $(".mantenimiento").css("display", "none")
             $(".perifericos").css("display", "none")
             $(".montar").css("display", "flex")
+            $(".info_montar").css("display", "flex")
+            $(".info_mantenimiento").css("display", "none")
+            $(".info_perifericos").css("display", "none")
             numero=0
         }
         else{
@@ -43,6 +62,9 @@ $(document).ready(function(){
                 $(".perifericos").css("display", "flex")
                 $(".mantenimiento").css("display", "none")
                 $(".montar").css("display", "none")
+                $(".info_montar").css("display", "none")
+                $(".info_mantenimiento").css("display", "none")
+                $(".info_perifericos").css("display", "flex")
                 numero=2
             }
             else{
@@ -51,6 +73,9 @@ $(document).ready(function(){
                     $(".mantenimiento").css("display", "flex")
                     $(".perifericos").css("display", "none")
                     $(".montar").css("display", "none")
+                    $(".info_montar").css("display", "none")
+                    $(".info_mantenimiento").css("display", "flex")
+                    $(".info_perifericos").css("display", "none")
                     numero=1
                 }
             }
