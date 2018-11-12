8fcaef8a643579c9d31d665da921f62ce8f0bf48
teamzz111 <aflr@outlook.com>
2018-11-11 23:50:14 -0500

    Solucionando problema de animaciones

--- js/js_tercero.js
+++ js/js_tercero.js
@@ -1,9 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".hola").fadeOut(0)
-    $(".iniciar").mouseenter(function(){
+    $(".iniciar").mouseenter(function () {
         $(".hola").fadeIn(1000)
     })
-    $(".iniciar").mouseleave(function(){
+    $(".iniciar").mouseleave(function () {
         $(".hola").fadeOut(1000)
     })
+
+    $('.registrarse').hover(function () {
+            $(this).animate({   
+                width: "100%"
+            })
+            $('.registrarse h1').animate({
+                margin: "-0.35em auto",
+            })
+        }, function () {    
+            $(this).animate({
+                width: "50%"
+            })
+            $('.registrarse h1').animate({
+                margin: "1.5em auto",
+            })
+        }
+
+    );
 })
