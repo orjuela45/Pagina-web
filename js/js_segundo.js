a7b6bf0718b503e415975cc24d37a7c54c8e42fa
orjuela45 <miguel.orjuela45@gmail.com>
2018-11-13 00:48:40 -0500

    responsives de pagina 1 2 y 4 terminados, paginas al 100% ya solo falta pagina 3

--- js/js_segundo.js
+++ js/js_segundo.js
@@ -1,5 +1,15 @@
 var estado=0;
 $(document).ready(function(){
+    $(".informacion_1 .informacion1").css("display","none")
+    $(".informacion_1 .informacion2").css("display","none")
+    $(".informacion_1 .informacion3").css("display","none")
+    $(".informacion_1 .informacion4").css("display","none")
+    $(".informacion_2 .informacion5").css("display","none")
+    $(".informacion_2 .informacion6").css("display","none")
+    $(".informacion_2 .informacion7").css("display","none")
+    $(".informacion_2 .informacion8").css("display","none")
+    $(".informacion_3 .informacion9").css("display","none")
+    $(".informacion_3 .informacion10").css("display","none")
     $(".procesador").click(function(){
         cerrar();
         if(estado==0)
@@ -25,7 +35,7 @@ $(document).ready(function(){
         {
             window.setTimeout(function(){
                 $(".informacion_1 .informacion2").slideDown(1500)
-            },1500)
+            },1000)
         }
     })
     $(".motherboard").click(function(){
@@ -39,7 +49,7 @@ $(document).ready(function(){
         {
             window.setTimeout(function(){
                 $(".informacion_1 .informacion3").slideDown(1500)
-            },1500)
+            },1000)
         }
     })
     $(".fuente").click(function(){
@@ -53,7 +63,7 @@ $(document).ready(function(){
         {
             window.setTimeout(function(){
                 $(".informacion_1 .informacion4").slideDown(1500)
-            },1500)
+            },1000)
         }
     })
     $(".disipador").click(function(){
@@ -67,7 +77,7 @@ $(document).ready(function(){
         {
             window.setTimeout(function(){
                 $(".informacion_2 .informacion5").slideDown(1500)
-            },1500)
+            },1000)
         }
     })
     $(".disco").click(function(){
@@ -81,7 +91,7 @@ $(document).ready(function(){
         {
             window.setTimeout(function(){
                 $(".informacion_2 .informacion6").slideDown(1500)
-            },1500)
+            },1000)
         }
     })
     $(".tarjeta").click(function(){
@@ -95,7 +105,7 @@ $(document).ready(function(){
         {
             window.setTimeout(function(){
                 $(".informacion_2 .informacion7").slideDown(1500)
-            },1500)
+            },1000)
         }
     })
     $(".chasis").click(function(){
@@ -109,7 +119,7 @@ $(document).ready(function(){
         {
             window.setTimeout(function(){
                 $(".informacion_2 .informacion8").slideDown(1500)
-            },1500)
+            },1000)
         }
     })
     $(".ventilador").click(function(){
@@ -123,7 +133,7 @@ $(document).ready(function(){
         {
             window.setTimeout(function(){
                 $(".informacion_3 .informacion9").slideDown(1500)
-            },1500)
+            },1000)
         }
     })
     $(".refrigeracion").click(function(){
@@ -137,19 +147,19 @@ $(document).ready(function(){
         {
             window.setTimeout(function(){
                 $(".informacion_3 .informacion10").slideDown(1500)
-            },1500)
+            },1000)
         }
     })
 })
 cerrar = function(){
-    $(".informacion_1 .informacion1").slideUp(1500)
-    $(".informacion_1 .informacion2").slideUp(1500)
-    $(".informacion_1 .informacion3").slideUp(1500)
-    $(".informacion_1 .informacion4").slideUp(1500)
-    $(".informacion_2 .informacion5").slideUp(1500)
-    $(".informacion_2 .informacion6").slideUp(1500)
-    $(".informacion_2 .informacion7").slideUp(1500)
-    $(".informacion_2 .informacion8").slideUp(1500)
-    $(".informacion_3 .informacion9").slideUp(1500)
-    $(".informacion_3 .informacion10").slideUp(1500)
+    $(".informacion_1 .informacion1").slideUp(1000)
+    $(".informacion_1 .informacion2").slideUp(1000)
+    $(".informacion_1 .informacion3").slideUp(1000)
+    $(".informacion_1 .informacion4").slideUp(1000)
+    $(".informacion_2 .informacion5").slideUp(1000)
+    $(".informacion_2 .informacion6").slideUp(1000)
+    $(".informacion_2 .informacion7").slideUp(1000)
+    $(".informacion_2 .informacion8").slideUp(1000)
+    $(".informacion_3 .informacion9").slideUp(1000)
+    $(".informacion_3 .informacion10").slideUp(1000)
 }
