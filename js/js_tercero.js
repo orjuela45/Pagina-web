09bc9b914c073753702bebf07a45310f606a45f3
orjuela45 <miguel.orjuela45@gmail.com>
2018-11-14 00:25:10 -0500

    avanzando pagina 3

--- js/js_tercero.js
+++ js/js_tercero.js
@@ -1,40 +1,123 @@
 $(document).ready(function () {
     $(".formulario").fadeOut(0)
     $(".formulario2").fadeOut(0)
-    $('.registrarse').hover(function () {
-            $(this).animate({   
-                width: "100%"
+    $(".registrarse").css("height", "50%")
+    $(".iniciar").css("height", "50%")
+    $(".modelos_motherboard,.modelos_ram,.modelos_fuente,.modelos_disipador,.modelos_discoduro,.modelos_tarjeta,.modelos_chasis,.modelos_ventiladores,.modelos_refrigeracion").css("display", "none")
+    if($(window).width()<800){
+        $(".registrarse").css("height", "15%")
+        $(".iniciar").css("height", "15%")
+        $('.registrarse').hover(function () {
+            $(this).animate({  
+                height: "60%" 
             })
             $('.registrarse h1').animate({
                 margin: "-0.03em auto",
             })
-            $(".formulario2").animate({heihgt: "toggle"},400)
+            $(".formulario2").fadeIn(300)
         }, function () {    
             $(this).animate({
-                width: "50%"
+                height:"15%"
             })
             $('.registrarse h1').animate({
-                margin: "1.5em auto",
+                margin: "0.2em auto",
             })
-            $(".formulario2").animate({heihgt: "toggle"},400)
-        }
-    );
-    $('.iniciar').hover(function () {
-        $(this).animate({   
-            width: "100%"
+            $(".formulario2").fadeOut(150)
+            }
+        );
+        $('.iniciar').hover(function () {
+            $(this).animate({   
+                height:"60%"
+            })
+            $('.iniciar h1').animate({
+                margin: "-0.03em auto",
+            })
+            $(".formulario").fadeIn(150)
+        }, function () {    
+            $(this).animate({
+                height: "15%"
+            })
+            $('.iniciar h1').animate({
+                margin: "0.2em auto",
+            })
+            $(".formulario").fadeOut(150)
         })
-        $('.iniciar h1').animate({
-            margin: "-0.03em auto",
-        })
-        $(".formulario").animate({width: "toggle"},400)
-    }, function () {    
-        $(this).animate({
-            width: "50%"
-        })
-        $('.iniciar h1').animate({
-            margin: "1.5em auto",
-        })
-        $(".formulario").animate({width: "toggle"},400)
     }
-);
+    else{
+        if($(window).width()<1100){
+            $('.registrarse').hover(function () {
+                $(this).animate({   
+                    width: "70%"
+                })
+                $('.registrarse h1').animate({
+                    margin: "-0.03em auto",
+                })
+                $(".formulario2").fadeIn(300)
+            }, function () {    
+                $(this).animate({
+                    width: "50%"
+                })
+                $('.registrarse h1').animate({
+                    margin: "1.5em auto",
+                })
+                $(".formulario2").fadeOut(150)
+                }
+            );
+            $('.iniciar').hover(function () {
+                $(this).animate({   
+                    width: "70%"
+                })
+                $('.iniciar h1').animate({
+                    margin: "-0.03em auto",
+                })
+                $(".formulario").fadeIn(150)
+            }, function () {    
+                $(this).animate({
+                    width: "50%"
+                })
+                $('.iniciar h1').animate({
+                    margin: "1.5em auto",
+                })
+                $(".formulario").fadeOut(150)
+            })
+        }
+        else{
+            $('.registrarse').hover(function () {
+                $(this).animate({   
+                    width: "100%"
+                })
+                $('.registrarse h1').animate({
+                    margin: "-0.03em auto",
+                })
+                $(".formulario2").fadeIn(300)
+            }, function () {    
+                $(this).animate({
+                    width: "50%"
+                })
+                $('.registrarse h1').animate({
+                    margin: "1.5em auto",
+                })
+                $(".formulario2").fadeOut(150)
+            }
+            );
+            $('.iniciar').hover(function () {
+                $(this).animate({   
+                    width: "100%"
+                })
+                $('.iniciar h1').animate({
+                    margin: "-0.03em auto",
+                })
+                $(".formulario").fadeIn(150)
+            }, function () {    
+                $(this).animate({
+                    width: "50%"
+                })
+                $('.iniciar h1').animate({
+                    margin: "1.5em auto",
+                })
+                $(".formulario").fadeOut(150)
+            }
+            );
+        }
+    }
 })
