ef9594cb068c030f7e6402fdd1306aa702f70f5b
orjuela45 <miguel.orjuela45@gmail.com>
2018-11-14 12:21:55 -0500

    implementacion nueva pagina

--- js/js_tercero.js
+++ js/js_tercero.js
@@ -1,12 +1,8 @@
 $(document).ready(function () {
     $(".formulario").fadeOut(0)
     $(".formulario2").fadeOut(0)
-    $(".registrarse").css("height", "50%")
-    $(".iniciar").css("height", "50%")
     $(".modelos_motherboard,.modelos_ram,.modelos_fuente,.modelos_disipador,.modelos_discoduro,.modelos_tarjeta,.modelos_chasis,.modelos_ventiladores,.modelos_refrigeracion").css("display", "none")
     if($(window).width()<800){
-        $(".registrarse").css("height", "15%")
-        $(".iniciar").css("height", "15%")
         $('.registrarse').hover(function () {
             $(this).animate({  
                 height: "60%" 
@@ -45,6 +41,9 @@ $(document).ready(function () {
     }
     else{
         if($(window).width()<1100){
+            $(".panel").css("height", "100%")
+            $(".registrarse").css("height", "50%")
+            $(".iniciar").css("height", "50%")
             $('.registrarse').hover(function () {
                 $(this).animate({   
                     width: "70%"
@@ -82,6 +81,7 @@ $(document).ready(function () {
             })
         }
         else{
+            $(".panel").css("height", "50%")
             $('.registrarse').hover(function () {
                 $(this).animate({   
                     width: "100%"
